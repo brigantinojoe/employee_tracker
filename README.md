2b46114eafeae031e9d13ba8ad030ea6ed39c767
brigantinojoe <103912190+brigantinojoe@users.noreply.github.com>
2022-12-21 23:28:07 -0800

    Add functions to handle new roles and employees. Add function to update employee but it has a bug.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# employee_tracker
+# Database Management - Employee Tracker
+
+## Description
+The purpose of this project is to execute CRUD operations on a database of departments, roles, and employees. I've learned how frustrating it is using inquirer and the mysql2 node package. 
+
+## Installation
+Go to your terminal, in the root direcory of the project, and run `npm i`. That will download the node packages necessary to run the application. Then run `node index.js` at the root directory and the application will begin with a few options.
+
+## Usage
+1. Run `node index.js` at the root directory of your project to start
+2. Choose one of the options listed in your terminal.
+3. Answer a few questions, and enjoy!
+
+## Video Tutorial
+Below is a video tutorial for how to use the application. If your a visual person, this one's for you!
+[![asciicast](./assets/terminal.jpg)](https://drive.google.com/file/d/1KMaV-XrbcwqAFfhwErB7yRMmq8r5U6A_/view)
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const Department = require('./lib/department.js');
 const Role = require('./lib/role.js');
 const db = require('./config/connection.js');
+const Employee = require("./lib/employee.js");
 
 const interval_restart = function () { setInterval(start_inquirer, 5000); };
 
@@ -23,7 +24,9 @@ const start_inquirer = function () {
                     : response.options === "view all employees" ? employee_table()
                         : response.options === "add a department" ? addDepartment()
                             : response.options === "add a role" ? addRole()
-                                : console.log("Nope");
+                                : response.options === "add an employee" ? addEmployee()
+                                    : response.options === "update an employee role" ? updateEmployee()
+                                        : console.log("Nope");
         }
         );
 };
@@ -94,11 +97,14 @@ const addDepartment = function () {
         .then((response) => {
             const new_department = new Department(response.department_name);
             new_department.addDepartmentSql(response.department_name);
+            start_inquirer();
         }
         );
 };
 
+var dept_array = [];
 var role_array = [];
+var manager_array = [];
 
 const addRole = function () {
     clearInterval(interval_restart);
@@ -119,7 +125,7 @@ const addRole = function () {
                 type: 'list',
                 message: 'Which department does the role belong to?',
                 name: 'departments',
-                choices: role_array
+                choices: dept_array
             }
         ])
         .then(async (response) => {
@@ -136,12 +142,121 @@ const addRole = function () {
         );
 };
 
+const addEmployee = function () {
+    clearInterval(interval_restart);
+    getRoles();
+    getManagers();
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the employee\'s first name?',
+                name: "first"
+            },
+            {
+                type: 'input',
+                message: 'What is the employee\'s last name?',
+                name: "last"
+            },
+            {
+                type: 'list',
+                message: 'What is the employee\'s role?',
+                name: 'role',
+                choices: role_array
+            },
+            {
+                type: 'list',
+                message: 'Who is the employee\'s manager?',
+                name: 'manager',
+                choices: manager_array
+            }
+        ])
+        .then(async (response) => {
+            const first = response.first;
+            const last = response.last;
+            const role = response.role;
+            const manager = response.manager;
+            const myRe = /[0-9]+/;
+            const roleRe = myRe.exec(role);
+            const managerRe = myRe.exec(manager);
+            const final_role = Number(roleRe[0]);
+            const final_manager = Number(managerRe[0]);
+            const newEmployee = new Employee(first, last, final_role, final_manager);
+            newEmployee.addEmployeeSql();
+            start_inquirer();
+        }
+        );
+};
+
+const updateEmployee = function () {
+    getRoles();
+    getManagers();
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Which employee\'s role would you like to update?',
+                name: 'id',
+                choices: manager_array
+            },
+            {
+                type: 'list',
+                message: 'Which role do you want to assign the selected employee?',
+                name: 'role',
+                choices: role_array
+            }
+        ])
+        .then(async (response) => {
+            const role = response.role;
+            const employee = response.id;
+            const myRe = /[0-9]+/;
+            const roleRe = myRe.exec(role);
+            const employeeRe = myRe.exec(employee);
+            const final_role = Number(roleRe[0]);
+            const final_employee = Number(employeeRe[0]);
+            // Add function call to update row
+            updateEmployeeSql(final_employee, final_role);
+            start_inquirer();
+        }
+        );
+};
+
 function getDepartments() {
-    role_array = [];
+    dept_array = [];
     db.query(`SELECT * FROM department`, function (err, results) {
         for (let i = 0; i < results.length; i++) {
             const element = results[i];
-            role_array.push(element.dept_name);
+            dept_array.push(element.dept_name);
+        }
+        return dept_array;
+    });
+}
+
+function getRoles() {
+    role_array = [];
+    db.query(`SELECT 
+	CONCAT(role.id, '. ', role.title, ': ', department.dept_name) as full_name
+    FROM
+    employees_db.role as role
+    LEFT JOIN employees_db.department as department
+    ON role.department_id = department.id`, function (err, results) {
+        for (let i = 0; i < results.length; i++) {
+            const element = results[i];
+            role_array.push(element.full_name);
+        }
+        return role_array;
+    });
+}
+
+function getManagers() {
+    manager_array = [];
+    db.query(`SELECT 
+    CONCAT(id,'. ',first_name,' ',last_name) AS full_name
+    FROM
+    employees_db.employee`, function (err, results) {
+        for (let i = 0; i < results.length; i++) {
+            const element = results[i];
+            manager_array.push(element.full_name);
         }
         return role_array;
     });
@@ -153,5 +268,12 @@ async function getId(department_name) {
         // console.log(err);
         console.log(results[0].id);
         return (results[0].id).valueOf();
+    });
+}
+
+function updateEmployeeSql(id, role) {
+    const values = [[id],[role]];
+    db.query("UPDATE employee SET role_id = ?? WHERE id = ?", [values], function (err, results) {
+        console.log(err);
     });
 }
